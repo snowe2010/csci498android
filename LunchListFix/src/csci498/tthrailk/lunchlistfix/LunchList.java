1603965b035eca4aab8593c307b44f67f243eb77
Tyler Thrailkill <tthrailk@mines.edu>
2012-09-03 00:50:51 -0600

    Implemented partial save button. It doesn't do anything yet, but it contains the handlers.

--- LunchListFix/src/csci498/tthrailk/lunchlistfix/LunchList.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/LunchList.java
@@ -1,15 +1,23 @@
 package csci498.tthrailk.lunchlistfix;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LunchList extends Activity {
-
+	
+	Restaurant r = new Restaurant();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button save = (Button) findViewById(R.id.save);
+        save.setOnClickListener(onSave);
     }
 
     @Override
@@ -17,4 +25,15 @@ public class LunchList extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    private View.OnClickListener onSave = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			EditText name = (EditText) findViewById(R.id.name);
+			EditText address = (EditText) findViewById(R.id.addr);
+			
+			r.setName(name.getText().toString());
+			
+		}
+	};
 }
