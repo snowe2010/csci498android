6cb5aa82c3e51a91d69ee3ebff12bd5b4df158c2
Tyler Thrailkill <tthrailk@mines.edu>
2012-09-03 02:01:03 -0600

    Added three awesome radio buttons for the extra credit. 4th radio button selected by default.

--- LunchListFix/src/csci498/tthrailk/lunchlistfix/LunchList.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/LunchList.java
@@ -1,21 +1,28 @@
 package csci498.tthrailk.lunchlistfix;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class LunchList extends Activity {
 	
 	Restaurant r = new Restaurant();
+	RadioGroup types;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        types = (RadioGroup) findViewById(R.id.types);
+        addRadioButtonListeners();
         
         Button save = (Button) findViewById(R.id.save);
         save.setOnClickListener(onSave);
@@ -27,6 +34,24 @@ public class LunchList extends Activity {
         return true;
     }
     
+    public void addRadioButtonListeners() {
+//    	RadioGroup extraCreditGroup = (RadioGroup) findViewById(R.id.types);
+    	 
+    	RadioButton ecButton1 = new RadioButton(this);
+    	ecButton1.setText(R.string.ec_radio_button_1);
+    	types.addView(ecButton1);
+    	 
+    	RadioButton ecButton2 = new RadioButton(this);
+    	ecButton2.setText(R.string.ec_radio_button_2);
+    	types.addView(ecButton2);
+    	 
+    	RadioButton ecButton3 = new RadioButton(this);
+    	ecButton3.setText(R.string.ec_radio_button_3);
+    	types.addView(ecButton3);
+    	 
+    	types.check(ecButton1.getId());
+    }
+    
     private View.OnClickListener onSave = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
@@ -36,8 +61,6 @@ public class LunchList extends Activity {
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
 			
-			RadioGroup types = (RadioGroup) findViewById(R.id.types);
-			    
 			switch (types.getCheckedRadioButtonId()) {
 				case R.id.sit_down:
 					r.setType("sit_down");
