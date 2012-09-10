67e6f43481e3f32639e9480c4c0c972258a8c341
Tyler Thrailkill <tthrailk@mines.edu>
2012-09-09 23:04:26 -0600

    Added a toast! Keeps current restaurant information.

--- LunchListFix/src/csci498/tthrailk/lunchlistfix/LunchList.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/LunchList.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -21,6 +22,7 @@ import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LunchList extends TabActivity {
 	
@@ -31,6 +33,7 @@ public class LunchList extends TabActivity {
 	EditText name 		= null;
 	EditText address	= null;
 	EditText notes 		= null;
+	Restaurant current	= null;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -73,42 +76,38 @@ public class LunchList extends TabActivity {
     private View.OnClickListener onSave = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			Restaurant r 		= new Restaurant();
-			EditText name 		= (EditText) findViewById(R.id.name);
-			EditText address 	= (EditText) findViewById(R.id.addr);
-			EditText notes		= (EditText) findViewById(R.id.notes);
-			
-			r.setName(name.getText().toString());
-			r.setAddress(address.getText().toString());
-			r.setNotes(notes.getText().toString());
+			current 		= new Restaurant();
+			current.setName(name.getText().toString());
+			current.setAddress(address.getText().toString());
+			current.setNotes(notes.getText().toString());
 			
 			switch (types.getCheckedRadioButtonId()) {
 				case R.id.sit_down:
-					r.setType("sit_down");
+					current.setType("sit_down");
 					break;
 				case R.id.take_out:
-					r.setType("take_out");
+					current.setType("take_out");
 					break;
 				case R.id.delivery:
-					r.setType("delivery");
+					current.setType("delivery");
 					break;
 			}
 			
-			adapter.add(r);
+			adapter.add(current);
 		}
 	};
 
 	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			Restaurant r = restaurantList.get(position);
-			name.setText(r.getName());
-			address.setText(r.getAddress());
-			notes.setText(r.getNotes());
+			current = restaurantList.get(position);
+			name.setText(current.getName());
+			address.setText(current.getAddress());
+			notes.setText(current.getNotes());
 			
-			if (r.getType().equals("sit_down")) {
+			if (current.getType().equals("sit_down")) {
 				types.check(R.id.sit_down);
 			}
-			else if (r.getType().equals("take_out")) {
+			else if (current.getType().equals("take_out")) {
 				types.check(R.id.take_out);
 			}
 			else {
@@ -124,9 +123,26 @@ public class LunchList extends TabActivity {
 	    return(super.onCreateOptionsMenu(menu));
 	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	  if (item.getItemId()==R.id.toast) {
+	    String message="No restaurant selected";
+	    
+	    if (current!=null) {
+	      message=current.getNotes();
+	    }
+	    
+	    Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+	    
+	    return(true);
+	  }
+	  
+	  return(super.onOptionsItemSelected(item));
+	}
+	
 	class RestaurantAdapter extends ArrayAdapter<Restaurant> {
 	RestaurantAdapter() {
-			super(LunchList.this, android.R.layout.simple_list_item_1, restaurantList);
+			super(LunchList.this, R.layout.row, restaurantList);
 		}
 		
 		public View getView(int position, View convertView, ViewGroup parent) {
@@ -136,7 +152,7 @@ public class LunchList extends TabActivity {
 			
 			if (row == null) {
 				LayoutInflater inflater = getLayoutInflater();
-				row = inflater.inflate(R.layout.row, null);
+				row = inflater.inflate(R.layout.row, parent, false);
 				holder = new RestaurantHolder(row);
 				row.setTag(holder);
 			}
