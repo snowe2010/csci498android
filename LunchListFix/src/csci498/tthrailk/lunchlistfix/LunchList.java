777514c7bdef07231be87f7ee88787d6a86817f4
Tyler Thrailkill <tthrailk@mines.edu>
2012-09-26 01:14:20 -0600

    Added settings icon and button to the menu

--- LunchListFix/src/csci498/tthrailk/lunchlistfix/LunchList.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/LunchList.java
@@ -19,17 +19,17 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class LunchList extends ListActivity {
-	
+
 	public final static String ID_EXTRA = "csci498.tthrailk._ID";
 	Cursor restaurantList;
 	RestaurantAdapter adapter;
 	RestaurantHelper helper;
-	
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         createListView();
     }
 
@@ -40,48 +40,51 @@ public class LunchList extends ListActivity {
     	adapter 		= new RestaurantAdapter(restaurantList);
     	setListAdapter(adapter);
     }
-    
+
     @Override
     public void onDestroy() {
     	super.onDestroy();
     	helper.close();
     }
-    
+
     @Override
     public void onListItemClick(ListView parent, View view, int position, long id) {
     	Intent i = new Intent(LunchList.this, DetailForm.class);
     	i.putExtra(ID_EXTRA, String.valueOf(id));
     	startActivity(i);
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	new MenuInflater(this).inflate(R.menu.option, menu);
     	return super.onCreateOptionsMenu(menu);
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	if (item.getItemId() == R.id.add) {
     		startActivity(new Intent(LunchList.this, DetailForm.class));
+    		return true;
+    	} else if (item.getItemId() == R.id.prefs) {
+    		startActivity(new Intent(this, EditPreferences.class));
     		return true;
     	}
     	return super.onOptionsItemSelected(item);
     }
 
 	class RestaurantAdapter extends CursorAdapter {
-		
+
 		RestaurantAdapter(Cursor c) {
 			super(LunchList.this, c);
 		}
-		
+
 		@Override
 		public void bindView(View row, Context context, Cursor c) {
 			RestaurantHolder holder = (RestaurantHolder) row.getTag();
 			holder.populateFrom(c, helper);
 		}
-		
-		@Override 
+
+		@Override
 		public View newView(Context context, Cursor c, ViewGroup parent) {
 			LayoutInflater inflater = getLayoutInflater();
 			View row = inflater.inflate(R.layout.row, parent, false);
@@ -90,22 +93,22 @@ public class LunchList extends ListActivity {
 			return row;
 		}
 	}
-	
+
 	static class RestaurantHolder {
 		private TextView name 		= null;
 		private TextView address 	= null;
 		private ImageView icon 		= null;
-		
+
 		RestaurantHolder(View row) {
 			name 	=	(TextView) row.findViewById(R.id.title);
 			address =	(TextView) row.findViewById(R.id.address);
 			icon 	= 	(ImageView) row.findViewById(R.id.icon);
 		}
-		
+
 		void populateFrom(Cursor c, RestaurantHelper helper) {
 			name.setText(helper.getName(c));
 			address.setText(helper.getAddress(c));
-			
+
 			if (helper.getType(c).equals("sit_down")) {
 				icon.setImageResource(R.drawable.ball_red);
 			}
@@ -118,4 +121,4 @@ public class LunchList extends ListActivity {
 		}
 	}
 }
-	
+
