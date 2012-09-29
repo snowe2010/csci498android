011e272174c0b2685fa8a20bd20ebe85fe494731
Tyler Thrailkill <tthrailk@mines.edu>
2012-09-29 15:52:52 -0600

    Added onSaveInstanceState for list values

--- LunchListFix/src/csci498/tthrailk/lunchlistfix/DetailForm.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/DetailForm.java
@@ -10,60 +10,60 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class DetailForm extends Activity {
-	
+
 	EditText name			= null;
 	EditText address		= null;
 	EditText notes			= null;
 	RadioGroup types		= null;
 	RestaurantHelper helper	= null;
 	String restaurantId 	= null;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.detail_form);
-	    
+
 	    helper 		= new RestaurantHelper(this);
 	    types 		= (RadioGroup) 	findViewById(R.id.types);
 	    name		= (EditText) 	findViewById(R.id.name);
 	    address		= (EditText) 	findViewById(R.id.addr);
 	    notes 		= (EditText) 	findViewById(R.id.notes);
 	    Button save = (Button) 		findViewById(R.id.save);
-	    
+
 	    save.setOnClickListener(onSave);
 	    restaurantId = getIntent().getStringExtra(LunchList.ID_EXTRA);
-	    
+
 	    if (restaurantId != null) {
 	    	load();
 	    }
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		helper.close();
 	}
-	
+
 	private void load() {
 		Cursor c = helper.getById(restaurantId);
 		c.moveToFirst();
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
-		
+
 		if (helper.getType(c).equals("sit_down")) {
 			types.check(R.id.sit_down);
 		} else if (helper.getType(c).equals("take_out")) {
 			types.check(R.id.take_out);
 		} else types.check(R.id.delivery);
-		
+
 		c.close();
 	}
-	
+
 	private View.OnClickListener onSave = new View.OnClickListener() {
 		public void onClick(View v) {
 			String type = null;
-			
+
 			switch (types.getCheckedRadioButtonId()) {
 			case R.id.sit_down:
 				type = "sit_down";
@@ -74,16 +74,26 @@ public class DetailForm extends Activity {
 			case R.id.delivery:
 				type = "delivery";
 				break;
-			
+
 			}
-			
+
 			if (restaurantId == null) {
 				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
 			} else {
 				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
 			}
-			
+
 			finish();
 		}
 	};
+
+	@Override
+	public void onSaveInstanceState(Bundle state) {
+		super.onSaveInstanceState(state);
+
+		state.putString("name", name.getText().toString());
+		state.putString("address", address.getText().toString());
+		state.putString("notes", notes.getText().toString());
+		state.putInt("type", types.getCheckedRadioButtonId());
+	}
 }
