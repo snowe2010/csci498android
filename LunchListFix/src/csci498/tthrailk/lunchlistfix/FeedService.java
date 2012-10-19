4bb64dd8a0719916c1992eee0cb5a1efe57862e9
Tyler Thrailkill <tthrailk@mines.edu>
2012-10-18 20:54:17 -0600

    APT 16: Step 2: Added onHandleIntent method implementation
    Added an RSSReader and RSSFeed

--- LunchListFix/src/csci498/tthrailk/lunchlistfix/FeedService.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/FeedService.java
@@ -1,18 +1,30 @@
 package csci498.tthrailk.lunchlistfix;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
 
+	public static final String EXTRA_URL = "csci498.tthrailk.EXTRA_URL";
+	
 	public FeedService() {
 		super("FeedService");
 	}
 
 	@Override
 	protected void onHandleIntent(Intent arg0) {
-		// TODO Auto-generated method stub
-
+		RSSReader reader=new RSSReader();
+		  
+		  try {
+		    RSSFeed result=reader.load(arg0.getStringExtra(EXTRA_URL));
+		  }
+		  catch (Exception e) {
+		    Log.e("LunchList", "Exception parsing feed", e);
+		  }
 	}
 
 }
