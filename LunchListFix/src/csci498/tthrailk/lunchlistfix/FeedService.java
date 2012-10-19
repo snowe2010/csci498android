539467d957a7813b73816eae7cea599093eedd23
Tyler Thrailkill <tthrailk@mines.edu>
2012-10-18 20:48:21 -0600

    APT 16: Step 1: Added FeedService.java and to the Manifest

---
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/FeedService.java
@@ -0,0 +1,18 @@
+package csci498.tthrailk.lunchlistfix;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+
+	public FeedService() {
+		super("FeedService");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
