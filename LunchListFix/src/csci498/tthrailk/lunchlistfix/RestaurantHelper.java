da5730b4075c8c4770d2e2dd05c975dab9e80162
Tyler Thrailkill <tthrailk@mines.edu>
2012-10-18 23:02:20 -0600

    APT 17: Step 1: Add Latitude and Longitude to the Data Model
    Added lat and lon columns to the database, changed schema version to 3,
    added upgrade method to upgrade old consumers to new database, added
    updateLocation method to update database with lat and lon locations.

--- LunchListFix/src/csci498/tthrailk/lunchlistfix/RestaurantHelper.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/RestaurantHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteQueryBuilder;
 class RestaurantHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_NAME 	= "lunchlist.db";
-	private static final int SCHEMA_VERSION 	= 2;
+	private static final int SCHEMA_VERSION 	= 3;
 
 	public RestaurantHelper(Context context) {
 		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
@@ -20,12 +20,19 @@ class RestaurantHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT);");
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT, lat REAL, lon REAL);");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
+		if (oldVersion < 2) {
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
+		}
+
+		if (oldVersion < 3) {
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN lat REAL");
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN lon REAL");
+		}
 	}
 
 	public void insert(String name, String address, String type, String notes, String feed) {
@@ -53,14 +60,24 @@ class RestaurantHelper extends SQLiteOpenHelper {
 		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
 	}
 
+	public void updateLocation(String id, double lat, double lon) {
+		ContentValues cv = new ContentValues();
+		String[] args = {id};
+
+		cv.put("lat", lat);
+		cv.put("lon", lon);
+
+		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
+	}
+
 	public Cursor getById(String id) {
 		String[] args = {id};
 
-		return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed FROM restaurants WHERE _ID=?", args);
+		return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed, lat, lon FROM restaurants WHERE _ID=?", args);
 	}
 
 	public Cursor getAll(String orderBy) {
-		return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed FROM restaurants ORDER BY " + orderBy, null);
+		return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed, lat, lon FROM restaurants ORDER BY " + orderBy, null);
 	}
 
 	public String getName(Cursor cursor) {
@@ -77,5 +94,11 @@ class RestaurantHelper extends SQLiteOpenHelper {
 	}
 	public String getFeed(Cursor cursor) {
 		return cursor.getString(5);
+	}
+	public double getLatitude(Cursor c ) {
+		return c.getDouble(6);
+	}
+	public double getLongitude(Cursor c) {
+		return c.getDouble(7);
 	}
 }
