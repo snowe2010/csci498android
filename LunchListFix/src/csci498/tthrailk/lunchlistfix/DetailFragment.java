884c0836b448e0d01dc9d91e926229c093d85ff1
Tyler Thrailkill <tthrailk@mines.edu>
2012-11-27 02:51:28 -0700

    APT 28: Step 7: Dial the Number
    Set up an option to call, from DetailForm. Only enables if the device
    is a telephone. Couldn't test this part, I do not have an android phone.

--- LunchListFix/src/csci498/tthrailk/lunchlistfix/DetailFragment.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/DetailFragment.java
@@ -8,6 +8,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -100,6 +101,9 @@ public class DetailFragment extends Fragment {
 			menu.findItem(R.id.location).setEnabled(false);
 			menu.findItem(R.id.map).setEnabled(false);
 		}
+		if (isTelephonyAvailable()) {
+			menu.findItem(R.id.call).setEnabled(true);
+		}
 	}
 
 	@Override
@@ -127,7 +131,13 @@ public class DetailFragment extends Fragment {
 			startActivity(i);
 
 			return true;
-		} else if (item.getItemId()==R.id.help) {
+		} else if (item.getItemId() == R.id.call) {
+			String toDial = "tel:" + phone.getText().toString();
+		    
+			if (toDial.length() > 4) {
+				startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(toDial)));
+			}
+		} else if (item.getItemId() == R.id.help) {
 			  startActivity(new Intent(getActivity(), HelpPage.class));
 		}
 
