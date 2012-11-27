065146d807927a2d2cb861304d8cd2dbf4df66b5
Tyler Thrailkill <tthrailk@mines.edu>
2012-11-26 19:09:59 -0700

    APT 25: Step 2: Create a Help Activity
    Created a HelpPage.java activity and also a help.xml layout file for that
    activity. Currently it loads the html file from the previous commit.

---
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/HelpPage.java
@@ -0,0 +1,21 @@
+package csci498.tthrailk.lunchlistfix;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class HelpPage extends Activity {
+
+	private WebView browser;
+	
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.help);
+    
+		browser = (WebView) findViewById(R.id.webkit);
+		browser.loadUrl("file:///android_asset/help.html");
+	}
+	
+}
