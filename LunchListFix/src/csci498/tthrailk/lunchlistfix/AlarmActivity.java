ade7f4110e622c6886cb74249225d18ec236a598
Tyler Thrailkill <tthrailk@mines.edu>
2012-11-26 23:37:34 -0700

    APT 28: Step 1: Add a Phone Number to the Database Schema
    Did exactly what it says above. Also changed the schema version.

--- LunchListFix/src/csci498/tthrailk/lunchlistfix/AlarmActivity.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/AlarmActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
--- LunchListFix/src/csci498/tthrailk/lunchlistfix/RestaurantHelper.java
+++ LunchListFix/src/csci498/tthrailk/lunchlistfix/RestaurantHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteQueryBuilder;
 class RestaurantHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_NAME = "lunchlist.db";
-	private static final int SCHEMA_VERSION = 3;
+	private static final int SCHEMA_VERSION = 4;
 
 	public RestaurantHelper(Context context) {
 		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
@@ -20,7 +20,7 @@ class RestaurantHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT, lat REAL, lon REAL);");
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT, lat REAL, lon REAL, phone TEXT);");
 	}
 
 	@Override
